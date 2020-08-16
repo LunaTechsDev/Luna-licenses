4e88f51b792734c1a44b6ae966bdcbfceb89599f
Kino  Rose <kinoarose@gmail.com>
2020-08-16 16:33:36 -0400

    Update main branch with fixes for development

---
+++ src/lunasurveyor/Luna_Surveyor.hx
@@ -0,0 +1,15 @@
+package lunasurveyor;
+
+import utils.Comment;
+
+@:nullSafety(Strict)
+class Luna_Surveyor {
+ public static function main() {
+  Comment.pluginParams("
+  @author Kino
+  @plugindesc An extension to the MV/MZ core that allows you to modify the in game ui
+  as a UI Kit <Luna_Surveyor>.");
+
+  Comment.title("Base Class Overrides");
+ }
+}
