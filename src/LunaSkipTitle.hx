95d01ea40ef657bfa7a46cf146a7b0faf23582c6
Kino  Rose <kinoarose@gmail.com>
2020-08-29 18:45:00 -0400

    Complete Luna Skip Title plugin

---
+++ src/LunaSkipTitle.hx
@@ -0,0 +1,15 @@
+import rm.scenes.Scene_Map;
+import rm.managers.SceneManager;
+import core.Types.JsFn;
+import rm.scenes.Scene_Title;
+import utils.Fn;
+
+class LunaSkipTitle {
+ public static function main() {
+  var oldsceneTitleStart: JsFn = Fn.getPrProp(Scene_Title, "start");
+  Fn.setPrProp(Scene_Title, "start", () -> {
+   oldsceneTitleStart.call(Fn.self);
+   SceneManager.goto(Scene_Map);
+  });
+ }
+}
